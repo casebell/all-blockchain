913612e6563acc1017e6f357026b6239c98ba99e
YOUNGSOO <guitaryc33@gmail.com>
2017-08-24 23:37:46 +0900

    add coin price change animation

--- src/main/webapp/app/home/coin-price/coin-price-row/coin-price-row.component.ts
+++ src/main/webapp/app/home/coin-price/coin-price-row/coin-price-row.component.ts
@@ -1,23 +1,51 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'abc-coin-price-row',
   templateUrl: './coin-price-row.component.html',
-  styleUrls: ['coin-price-row.scss']
+  styleUrls: ['coin-price-row.scss'],
+    animations: [
+        trigger('valueUpdated',[
+            transition(
+                ':enter', [
+                    style({backgroundColor:'#eee', transform: 'translateY(100%)', opacity: 0}),
+                    animate('500ms', style({backgroundColor:'#fff',transform: 'translateY(0)', opacity: 1}))
+                ]
+            ),
+            transition(
+                ':leave', [
+                    style({backgroundColor:'#fff',transform: 'translateY(0)', 'opacity': 1}),
+                    animate('500ms', style({backgroundColor:'#eee',transform: 'translateY(-100%)', opacity: 0}))
+                ]
+            )
+        ])
+    ]
 })
 export class CoinPriceRowComponent implements OnInit {
   @Input() coinRow;
   @Input('myCurrency') myCurrency;
   @Input('colsNum') colsNum;
-  rowTest = []
   CURRENCY_QUERY = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDKRW%22%2C%20%22EURKRW%22%2C%22CNYKRW%22%2C%22JPYKRW%22%2C%22EURUSD%22%2C%22CNYUSD%22%2C%22JPYUSD%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
   //select * from yahoo.finance.xchange where pair in ("USDKRW", "EURKRW","CNYKRW","JPYKRW","EURUSD","CNYUSD","JPYUSD")
   constructor(
     private http: HttpClient) { }
-  exchangeRate
+  exchangeRate;
+
+   /* @Input() set setCoinrow(value) {
+        this._coinRow = value;
+        this.coinRowAnimation(this._coinRow);
+
+    }
+
+    get coinRow(){
+
+        return this._coinRow;
+
+    }*/
+
   ngOnInit() {
-    this.rowTest = this.coinRow.coins;
     this.http.get(this.CURRENCY_QUERY)
       .subscribe((data: any) => {
         //console.log('currncy coin price row :', data);
@@ -25,4 +53,7 @@ export class CoinPriceRowComponent implements OnInit {
       });
   }
 
+  coinRowAnimation(value){
+
+  }
 }
